0073c07e76b05f9d8b3bb5753a4774a7b14763aa
Jheot <josueinfoctt@gmail.com>
2025-05-03 19:25:41 +0000

    T4

--- xadrez.c
+++ xadrez.c
@@ -1,33 +1,71 @@
 #include <stdio.h>
 
+void MovTorre (int movimentos){
+    if (movimentos > 0) {
+        printf("Direita\n");
+        MovTorre(movimentos - 1);
+    }
+}
+    
+
+void MovBispo (int movimentos){
+    if(movimentos > 0){
+        printf("Cima, direita\n");
+        MovBispo(movimentos - 1);
+    }
+}
+
+void MovRainha (int movimentos){
+    if(movimentos >0){
+        printf("Esquerda\n")
+        MovRainha(movimentos - 1);
+    }
+}
+
 int main() {
     // Simulação do movimento da Torre
-    int torre_movimento = 5;
     printf("Movimento da Torre:\n");
-    for (int i = 0; i < torre_movimento; i++) {
-        printf("Direita\n");
-    }
+    MovTorre(5);
+    printf("\n");
+   
+    // Simulação do movimento do Bispo
+    printf("Movimento do Bispo:\n");
+    MovBispo(5);
+    printf("\n");
+    
+
+    int rainha_movimento = 8;
+    int cavalo_movimento = 3;
+    
+   
     printf("\n");
 
     // Simulação do movimento do Bispo
-    int bispo_movimento = 5;
-    printf("Movimento do Bispo:\n");
-    int j = 0;
-    while (j < bispo_movimento) {
-        printf("Cima, Direita\n");
-        j++;
-    }
+    
+  
     printf("\n");
 
     // Simulação do movimento da Rainha
-    int rainha_movimento = 8;
+    
     printf("Movimento da Rainha:\n");
     int k = 0;
+        
     do {
         printf("Esquerda\n");
         k++;
     } while (k < rainha_movimento);
     printf("\n");
+
+    //Simulando moviemento do cavalo
+
+    for (int l = 1; l < cavalo_movimento; l++){
+        while (l < cavalo_movimento){
+            printf("Baixo\n");
+            l++;
+        }
+        printf("Esquerda\n");
+      
+    }
 
     return 0;
 }
